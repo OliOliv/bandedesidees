10bf8db61afcb6379513d178bf769707b6e5ba4e
OliviaZ <36442360+OliOliv@users.noreply.github.com>
2020-09-16 10:52:36 +0200

    add react testing library configuration

--- src/pages/HomePage/__tests__/index.spec.js
+++ src/pages/HomePage/__tests__/index.spec.js
@@ -1,11 +1,23 @@
-import { mount } from "enzyme";
+import { render, screen } from "@testing-library/react";
 
 import HomePage from "../";
+import { mount } from "enzyme";
 
 describe("HomePage", () => {
   it("should render more information button", () => {
     const wrapper = mount(<HomePage />);
 
     expect(wrapper.find("Button")).toHaveLength(1);
+  });
+
+  it("should render the button text", () => {
+    // Given
+    const buttonText = "Plus d'information";
+
+    // When
+    render(<HomePage />);
+
+    // Then
+    expect(screen.getByText(buttonText)).toBeInTheDocument();
   });
 });
--- tests/setupAfterEnv.js
+++ tests/setupAfterEnv.js
@@ -1,3 +1,5 @@
 const Adapter = require("enzyme-adapter-react-16");
 
+import "@testing-library/jest-dom/extend-expect";
+
 require("enzyme").configure({ adapter: new Adapter() });
